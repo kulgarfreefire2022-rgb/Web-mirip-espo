d53298536bfef01b1e9028b247c20192861743c9
Maukepoyahhh <kulgarfreefire2022@gmail.com>
2026-03-28 16:04:22 +0700

    Add create account response to data_api.php

--- data_api.php
+++ data_api.php
@@ -1,1 +1,16 @@
+<?php
+header('Content-Type: application/json');
+$action = $_POST['action'] ?? '';
+
+// Contoh respon saat user membeli "Create Account Auto King"
+if ($action == 'create_account_auto_king') {
+    echo json_encode([
+        "success" => true,
+        "email" => "espo_" . rand(100,999) . "@game.com",
+        "password" => "king" . rand(1000,9999)
+    ]);
+} else {
+    echo json_encode(["success" => true, "balance" => 10000, "is_vip" => false]);
+}
+?>
 
